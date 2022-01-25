3d09ecae7be17910756af71bfdf947201ac664cd
avisri786 <smartyavi786@gmail.com>
2022-01-25 15:44:00 +0530

    Updates

---
+++ precache-manifest.519de186f5d57579957d3e0671358a00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4579a6f0cad6a31cd7bbe8d9bc364ff6",
+    "url": "/Text-Tools-App/index.html"
+  },
+  {
+    "revision": "6964f24c024bb0bd7bc3",
+    "url": "/Text-Tools-App/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fb78a9ff9d70916843f6",
+    "url": "/Text-Tools-App/static/js/2.82eeb3fb.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Text-Tools-App/static/js/2.82eeb3fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20af6fb6b8770dfefda",
+    "url": "/Text-Tools-App/static/js/3.152cc5e7.chunk.js"
+  },
+  {
+    "revision": "6964f24c024bb0bd7bc3",
+    "url": "/Text-Tools-App/static/js/main.86ae9e29.chunk.js"
+  },
+  {
+    "revision": "c8e29013ef23c1c8a318",
+    "url": "/Text-Tools-App/static/js/runtime-main.d9bd2d7e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Text-Tools-App/static/media/logo.06e73328.svg"
+  }
+]);
